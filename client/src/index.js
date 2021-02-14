f28f4654b5bfba7544d58b0cb3cb1c4fa949e8ad
Kevin Miller <kmillergit@outlook.com>
2021-02-14 12:28:39 -0800

    removed deprecated watch-sass script

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/css-output/output.css';
+import './assets/index.css';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
