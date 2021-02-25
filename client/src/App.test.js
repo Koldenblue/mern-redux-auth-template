95afd31eaaa4c61067d80785828f9586b43620b0
Kevin Miller <kmillergit@outlook.com>
2021-02-25 09:35:35 -0800

    setup jest testing. added section and cover screen for loading

---
+++ client/src/App.test.js
@@ -0,0 +1,16 @@
+import { getByTestId, render, screen, toBeInTheDocument } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import App from './App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+
+test('renders', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  expect(getByTestId(document.documentElement, 'App')).toBeInTheDocument();
+});
+
+// learn more: https://github.com/testing-library/jest-dom
--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -4,12 +4,15 @@ import ReactLoading from "react-loading";
 export default function Loading() {
 
   return (
-    <div className='loading'>
-      <div className='loading-bottom'>
-        <div className='loading-bars'>
-          <ReactLoading color="rgb(180, 180, 180)" height={200} width={200} type="cubes" />
+    <section className='loading-component'>
+      <div className='screen-cover'></div>
+      <div className='loading'>
+        <div className='loading-bottom'>
+          <div className='loading-bars'>
+            <ReactLoading color="rgb(180, 180, 180)" height={200} width={200} type="cubes" />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
